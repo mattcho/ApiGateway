28b6159cf6820bb5fb2ac77d805a72798fac6c93
Ubuntu <ubuntu@ip-172-30-3-26.ec2.internal>
2017-02-22 16:28:12 +0000

    @EnableDiscoveryClient added

--- src/main/java/dongukc/ApiGateway2Application.java
+++ src/main/java/dongukc/ApiGateway2Application.java
@@ -3,6 +3,7 @@ package dongukc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ApiGateway2Application {
 
